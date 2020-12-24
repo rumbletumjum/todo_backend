177a33fcef5764c09019c0e14877a44d4f725c5a
Ron <rumbletumjum@gmail.com>
2020-12-24 01:19:34 -0500

    extract server & service

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -13,10 +14,33 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
+type TodoService interface {
+	getAll() ([]*Todo, error)
+	save(todo *Todo) error
+}
+
 type InMemoryTodoService struct {
 	sync.Mutex
 	nextId int
 	Todos  []*Todo
+}
+
+func (s *InMemoryTodoService) getAll() ([]*Todo, error) {
+	log.Printf("svc: getAll")
+	return s.Todos, nil
+}
+
+func (s *InMemoryTodoService) save(todo *Todo) error {
+	log.Printf("svc: save %v", todo)
+	s.Lock()
+	defer s.Unlock()
+	if todo.ID == 0 {
+		todo.ID = s.nextId
+		s.nextId++
+		s.Todos = append(s.Todos, todo)
+		return nil
+	}
+	return fmt.Errorf("unable to save")
 }
 
 func NewTodoService() *InMemoryTodoService {
@@ -28,15 +52,20 @@ func NewTodoService() *InMemoryTodoService {
 	return s
 }
 
-func (s *InMemoryTodoService) getAll(w http.ResponseWriter, r *http.Request) {
+type TodoServer struct {
+	svc TodoService
+}
+
+func (srv *TodoServer) getAll(w http.ResponseWriter, r *http.Request) {
 	log.Printf("getAll: %v", r)
-	err := json.NewEncoder(w).Encode(s.Todos)
+	todos, err := srv.svc.getAll()
+	err = json.NewEncoder(w).Encode(todos)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 	}
 }
 
-func (s *InMemoryTodoService) save(w http.ResponseWriter, r *http.Request) {
+func (srv *TodoServer) save(w http.ResponseWriter, r *http.Request) {
 	log.Printf("save: %v", r)
 	var todo Todo
 	err := json.NewDecoder(r.Body).Decode(&todo)
@@ -44,31 +73,24 @@ func (s *InMemoryTodoService) save(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	if todo.ID == 0 {
-		s.Lock()
-		defer s.Unlock()
-		todo.ID = s.nextId
-		s.nextId++
-
-		s.Todos = append(s.Todos, &todo)
-	}
+	err = srv.svc.save(&todo)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 	}
 }
 
-func (s *InMemoryTodoService) handle(w http.ResponseWriter, r *http.Request) {
-    switch r.Method {
-    case "GET":
-        s.getAll(w, r)
-    case "POST":
-        s.save(w, r)
-    }
+func (srv *TodoServer) handle(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		srv.getAll(w, r)
+	case "POST":
+		srv.save(w, r)
+	}
 }
 
 func main() {
-	svc := NewTodoService()
+	srv := TodoServer{svc: NewTodoService()}
 
-	http.HandleFunc("/todo", svc.handle)
+	http.HandleFunc("/todo", srv.handle)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
